2a2b795b23cbc093b696ea8a47c852ea84395a8f
jxczqr <artem.ilin.gm@gmail.com>
2022-05-01 05:21:16 +0300

    Update

---
+++ gifs.js
@@ -0,0 +1,7 @@
+export default {
+  bitcoin: 'https://i.giphy.com/media/4KFvuA2LmYYVv46CNN/giphy.webp',
+  money: 'https://media.giphy.com/media/1APhDggUPlkRdK5w1n/giphy.gif',
+  bb: 'https://media4.giphy.com/media/LNEtgKBGwdIvS/giphy.gif?cid=ecf05e4757sh1ktwah7wtjgiduqzfgdfnhtyidjiplr3pmy1&rid=giphy.gif&ct=g',
+  shib: 'https://media4.giphy.com/media/oBQZIgNobc7ewVWvCd/giphy.gif?cid=ecf05e47z3zm0s5dmxvmyxiws62kirke00qk0bvzbp58km6f&rid=giphy.gif&ct=g',
+  up: 'https://media0.giphy.com/media/50OuNGJcwIVXcuJURc/giphy.gif?cid=ecf05e47fd0r3ci6eemibd46wnfwxpumlg8c0v0vm5ujoh71&rid=giphy.gif&ct=g'
+}
